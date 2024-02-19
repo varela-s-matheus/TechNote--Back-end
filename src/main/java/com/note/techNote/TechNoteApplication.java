f1e22545673887af5b0e3fcb2743cb1c75dde716
Matheus Varela <matheus_s_varela@hotmail.com>
2024-02-18 21:30:49 -0300

    Ajustando arquivos da aplicação e retirando metodo de Commandline não utilizado

--- src/main/java/com/note/techNote/TechNoteApplication.java
+++ src/main/java/com/note/techNote/TechNoteApplication.java
@@ -1,21 +1,11 @@
 package com.note.techNote;
 
-import com.note.techNote.pricipal.Principal;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TechNoteApplication /*implements CommandLineRunner*/ {
-
+public class TechNoteApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TechNoteApplication.class, args);
 	}
-
-//	@Override
-//	public void run(String... args) throws Exception {
-//		Principal principal = new Principal();
-//
-//
-//	}
 }
--- src/main/java/com/note/techNote/pricipal/Principal.java
+++
@@ -1,6 +0,0 @@
-package com.note.techNote.pricipal;
-
-
-public class Principal {
-
-}
